0d8dd2a0d9a6f5c8bbef875a276b2d10448afafb
Siarhei Charepka <sergei1770984@gmail.com>
2020-09-04 18:31:21 +0300

    sayHi

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,7 +1,29 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int c = calculator.add(5, 5);
+        System.out.println(c);
+        int d = calculator.minus(10, 10);
+        System.out.println(d);
+        long f = calculator.multiply(10, 10);
+        System.out.println(f);
+        double j = calculator.division(10, 10);
+        System.out.println(j);
+
+
+        // Game
+        System.out.println("Введите число от 1 до 10");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int compNumber = (int) (Math.random()* 10);
+
+        if (number == compNumber) {
+            System.out.println("Комп угадал ваше число ");}
+        else {System.out.println("Комп не угадал ваше число");}
 
     }
 }
---
+++ src/main/java/com/company/util/MainChecker.java
@@ -0,0 +1,9 @@
+package com.company.util;
+
+public class MainChecker {
+    public static void main(String[] args) {
+        Checker checker = new Checker();
+        checker.isEven(2);
+        checker.isOdd(8);
+    }
+}
